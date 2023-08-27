2dc4fd9c44c53358b6c4cf3281e442177cce9142
ChexkHub <125713591+Polchawan@users.noreply.github.com>
2023-08-28 03:54:11 +0700

    Add files via upload

---
+++ enroll_withdraw.php
@@ -0,0 +1,20 @@
+<?php
+include '../conn.php';
+header("Location: ../enroll.php");
+//header =  กลับไปหน้าเดิม
+
+$id = $_GET['userID'];
+$sql = "DELETE FROM registra WHERE reg_id='$id' ";
+//echo $sql;
+$result = mysqli_query($conn,$sql);
+
+if($result){
+    echo "<script>alert('ลบข้อมูลเรียบร้อย');</script>";
+    echo "<script>window.location='enroll.php';</script>";
+}else{
+    echo "<script>alert('ไม่สามารถลบข้อมูลได้');</script>";
+}
+
+mysqli_close($conn)
+
+?>
